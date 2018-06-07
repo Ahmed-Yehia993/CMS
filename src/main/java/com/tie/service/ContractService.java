1281fc1f0e74edf2e87e0dfc2f6153adae46eb28
Ahmed El-Deeb <eng.ahmedeldeeeb@gmail.com>
2018-06-07 08:57:53 +0300

    add contract repo and service

---
+++ src/main/java/com/tie/service/ContractService.java
@@ -0,0 +1,11 @@
+package com.tie.service;
+
+import java.util.List;
+
+import com.tie.model.Contract;
+
+public interface ContractService {
+	public void saveContract(Contract contract);
+	public List<Contract> findAll();
+	Contract findOne();
+}
